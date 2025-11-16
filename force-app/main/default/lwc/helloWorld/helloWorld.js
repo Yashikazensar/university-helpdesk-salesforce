dd090e398cc18a988b5888c49b62d2d1bb0b1c0d
Yashika Shukla <yashika.shukla@zensar.com>
2025-11-16 20:27:50 +0530

    Initial

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    hello = 'Hello World';
+    handleClick() {
+        this.hello = 'Hello Yashi';
+    }
+}
